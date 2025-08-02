4b3323e272e7371a5f3d09eb70de9e0d6abbb514
Rudraneil <rudraneil123sarkar@gmail.com>
2025-08-02 08:40:46 +0530

    Added Character Selection + BG music

---
+++ Assets/_Script/CharacterSelector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public Material[] materials;
+    public GameObject Player;
+    public GameObject Prefab;
+    public GameObject Daruma;
+
+    private int currentMaterialIndex = 0;
+    private Renderer playerRenderer;
+
+    private void Start()
+    {
+        if (Player != null)
+        {
+            playerRenderer = Player.GetComponentInChildren<Renderer>();
+            if (playerRenderer != null && materials.Length > 0)
+            {
+                ApplyMaterial();
+            }
+        }
+    }
+
+    public void NextMaterial()
+    {
+        if (materials.Length == 0 || playerRenderer == null)
+            return;
+
+        currentMaterialIndex = (currentMaterialIndex + 1) % materials.Length;
+        ApplyMaterial();
+    }
+
+    private void ApplyMaterial()
+    {
+        playerRenderer.material = materials[currentMaterialIndex];
+    }
+
+    public void ConfirmSelection()
+    {
+        if (Prefab != null && materials.Length > 0)
+        {
+            Renderer prefabRenderer = Prefab.GetComponentInChildren<Renderer>();
+            if (prefabRenderer != null)
+            {
+                prefabRenderer.material = materials[currentMaterialIndex];
+            }
+            Player.SetActive(false);
+            Daruma?.SetActive(true);
+            Renderer DarumaRenderer = Daruma.GetComponent<Renderer>();
+            DarumaRenderer.material = materials[currentMaterialIndex];
+        }
+    }
+
+}
